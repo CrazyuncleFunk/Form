093cbbde09806c4506ef1441936641730a7fe658
Erin Dunn <erindunn@live.co.uk>
2019-10-08 07:17:30 +0100

    o

--- contact.php
+++ contact.php
@@ -1,3 +1,23 @@
+<?php
+if(isset($_POST['submit'])){
+    $to = 'erindunn@live.co.uk';
+    $from = $_POST['email'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2);
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+
+    }
+?>
+<!DOCTYPE html>
 <head>
 <link rel="stylesheet" href="..\Form\contact.css">
 <link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet">
@@ -21,24 +41,16 @@
   <div class="container">
 
     <h1 class="contact">Get In Touch </h1>
-    <form method="post" name="myemailform" id="myemailform" action="working.php">
-      <label for="name">Name: <input type="text" class="feild" name="name" id="name">
+    <form method="post" name="myemailform" id="myemailform" action="">
+      <label for="name">Name: <input type="text" class="feild" name="first_name" id="name">
+      <label for="name">Name: <input type="text" class="feild" name="last_name" id="name">
       <label for="email">Email: <input type="email" class="feild" name="email" id="email">
       <textarea class="textarea" name="message" id="textarea"></textarea>
       <button type="submit" class="btn">Submit</button>
     </form>
 
 </div>
-<?php
-  $name = $_POST['name'];
-  $visitor_email = $_POST['email'];
-  $message = $_POST['message'];
-  $email_from = 'erindunn@live.co.uk';
-  $email_subject = "New Form submission";
-  $email_body = "You have received a new message from $name";
-  $to = 'erindunn@live.co.uk';
-  $headers = "From: $email_from \r\n";
-  $headers .= "Reply-To: $visitor_email \r\n";
-  mail($to,$email_subject,$email_body,$headers);
-?>
+
+
+
 </body>
